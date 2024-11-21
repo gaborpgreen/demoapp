fe1599d1426e71cb8dc3b93d99a2cc44eedbb28a
Peltzer Gábor <gabor.peltzer@greenventions.hu>
2024-11-21 13:03:03 +0100

    ci

--- helm.sh
+++ helm.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-sed -i 's/\[app-version\]/'"${{ github.ref_name }}"'/g' helm/Chart.yaml
+echo 1
+echo $env.REGISTRY
+echo 2
+echo $REGISTRY
 
-helm package helm
-echo $GITHUB_TOKEN_REGISTRY | helm registry login ${{ env.REGISTRY }}/${{ github.GITHUB_ACTOR_ID }} --username ${{ github.GITHUB_ACTOR_ID }} --password-stdin
-ls
+#sed -i 's/\[app-version\]/'"${{ github.ref_name }}"'/g' helm/Chart.yaml
+
+#helm package helm
+#echo $GITHUB_TOKEN_REGISTRY | helm registry login ${{ env.REGISTRY }}/${{ github.GITHUB_ACTOR_ID }} --username ${{ github.GITHUB_ACTOR_ID }} --password-stdin
+
 #helm push ./istio-httpbin2-0.1.0.tgz oci://${{ env.REGISTRY }}/${{ github.GITHUB_ACTOR_ID }}
 #${{ github.repository }}
